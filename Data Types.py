8b3dd7a2243015df0f251d8eb2c7cac62fd41219
Somasekhar <somasekharravada@gmail.com>
2023-01-27 13:15:39 +0530

    Intro Data Types

---
+++ Data Types.py
@@ -0,0 +1,10 @@
+# Variable can store data of different types.
+
+# Text type: str
+# Numeric type: int, float, complex
+# Sequence type: list, tuple, range
+# Mapping type: dict
+# set type: set, frozonset
+# boolean type: bool
+# Binary type: bytes, bytearray, memoryview
+# None type: Nonetype
