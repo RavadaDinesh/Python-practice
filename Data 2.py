651db3a88697eac764b5d0b32231ae078dd828cd
Somasekhar <somasekharravada@gmail.com>
2023-01-27 13:32:25 +0530

    Data prac 2

---
+++ Data 2.py
@@ -0,0 +1,20 @@
+# List
+# x = ["apple", "banana", "orange"]  list are orderd
+# print(type(x))  mutable
+
+# Tuple
+# x = ("apple", "banana", "orange") ordered but immutiable
+# print(type(x))
+
+# Range
+# x = range(6)
+# print(x)
+# print(type(x))
+
+# dict
+# x = {"name" : "Dinesh", "age" : 36}
+# print(type(x))
+
+# set
+x = {"apples","banana","orange"}
+print(type(x))
